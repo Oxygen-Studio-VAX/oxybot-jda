a02c6f7440d5dc61b71989067e4a0e4713e858ff
Gizuzu <55207603+ustokan@users.noreply.github.com>
2022-12-12 18:52:26 +0300

    Update DatabaseService.kt

--- src/main/kotlin/su/gachi/services/DatabaseService.kt
+++ src/main/kotlin/su/gachi/services/DatabaseService.kt
@@ -5,7 +5,7 @@ import com.mongodb.client.MongoClients
 import com.mongodb.client.MongoCollection
 import com.mongodb.client.MongoDatabase
 import org.bson.Document
-import su.gachi.bot.core.Client
+import su.gachi.core.Client
 
 class DatabaseService(discordClient: Client) {
     val client: MongoClient
