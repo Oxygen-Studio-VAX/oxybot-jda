d0ed5364f6637caac7699a20f5af2bd3039c3f4d
Gizuzu <55207603+ustokan@users.noreply.github.com>
2022-12-14 15:44:08 +0300

    Add locales

--- src/main/kotlin/su/gachi/core/settings/GuildSettings.kt
+++ src/main/kotlin/su/gachi/core/settings/GuildSettings.kt
@@ -4,4 +4,5 @@ import net.dv8tion.jda.api.entities.Guild
 import su.gachi.core.Client
 
 class GuildSettings(val client: Client, val guild: Guild?) {
+    val general = GeneralSettings(client, guild)
 }
