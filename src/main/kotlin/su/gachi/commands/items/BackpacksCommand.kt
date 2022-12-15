3ae6f5166c0375ac72d821c6cdb024ce09586045
Gizuzu <55207603+ustokan@users.noreply.github.com>
2022-12-15 13:16:27 +0300

    Add backpack info

---
+++ src/main/kotlin/su/gachi/commands/items/BackpacksCommand.kt
@@ -0,0 +1,98 @@
+package su.gachi.commands.items
+
+import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.interactions.DiscordLocale
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.interactions.commands.build.OptionData
+import org.json.JSONObject
+import su.gachi.Config
+import su.gachi.core.commands.Command
+import su.gachi.core.commands.CommandContext
+import su.gachi.items.ArmoryItems
+import su.gachi.items.BackpacksItems
+import su.gachi.items.NpcsData
+
+class BackpacksCommand : Command() {
+    init {
+        name = "backpacks"
+        description = "Information about backpacks"
+
+        options = listOf(
+            OptionData(OptionType.STRING, "id", "Backpack ID (short name)", true, true)
+                .setDescriptionLocalization(DiscordLocale.RUSSIAN, "ID рюкзака (короткое имя)")
+        )
+    }
+
+    override fun handle(ctx: CommandContext) {
+        ctx.deferReply()
+
+        if (ctx.getOption("id")!!.asString == "list") {
+            val builder = StringBuilder()
+            BackpacksItems.raw.forEach {
+                val item = it as JSONObject
+                builder.append("`${item.getString("id")}` = **")
+                builder.append(
+                    if (item.getJSONObject("name_localized").has(ctx.locale))
+                        item.getJSONObject("name_localized").getString(ctx.locale)
+                    else
+                        item.getString("name")
+                )
+                builder.append("**\n")
+            }
+
+            ctx.editOriginalEmbeds(
+                EmbedBuilder().setColor(Config.embedColor)
+                    .setTitle(ctx.translate("commands.backpacks.title"))
+                    .setDescription(builder.toString())
+                    .setFooter(ctx.translate("commands.backpacks.footer"))
+                    .build()
+            )
+        } else {
+            val item = BackpacksItems.findByID(ctx.getOption("id")!!.asString)
+                ?: return ctx.editOriginalWithError(ctx.translate("commands.backpacks.error"))
+
+            val itemName = if (item.getJSONObject("name_localized").has(ctx.locale))
+                item.getJSONObject("name_localized").getString(ctx.locale)
+            else
+                item.getString("name")
+            val itemDescription = if (item.getJSONObject("description_localized").has(ctx.locale))
+                item.getJSONObject("description_localized").getString(ctx.locale)
+            else
+                item.getString("description")
+            val itemRarity = if (item.getJSONObject("rarity_localized").has(ctx.locale))
+                item.getJSONObject("rarity_localized").getString(ctx.locale)
+            else
+                item.getString("rarity")
+            val itemDemand = if (item.getJSONObject("demand_localized").has(ctx.locale))
+                item.getJSONObject("demand_localized").getString(ctx.locale)
+            else
+                item.getString("demand")
+
+            val pricingKeys = item.getJSONObject("pricing").keySet()
+                .filter { key -> !item.getJSONObject("pricing").isNull(key) }
+            val pricing = mutableMapOf<String, Any>()
+            pricingKeys.forEach { key -> pricing[key] = item.getJSONObject("pricing").get(key) }
+
+            if (!item.isNull("npc")) {
+                val npc = NpcsData.npcs[item.getString("npc")]
+                if (npc != null) {
+                    pricing["npc"] = "${npc.getString("emoji")} ${if (npc.getJSONObject("name_localized").has(ctx.locale))
+                        npc.getJSONObject("name_localized").getString(ctx.locale)
+                    else
+                        npc.getString("name")}"
+                }
+            }
+
+            ctx.editOriginalEmbeds(
+                EmbedBuilder().setColor(if (item.isNull("color")) Config.embedColor else item.getInt("color"))
+                    .setTitle(ctx.translate("commands.backpacks.info.title", mapOf("name" to itemName)))
+                    .setDescription(itemDescription)
+                    .setThumbnail(item.getString("image"))
+                    .addField(ctx.translate("commands.backpacks.info.pricing.name"), ctx.translate("commands.backpacks.info.pricing.value", pricing), true)
+                    .addField(ctx.translate("commands.backpacks.info.rarity.name"), itemRarity, true)
+                    .addField(ctx.translate("commands.backpacks.info.demand.name"), itemDemand, true)
+                    .build()
+            )
+        }
+    }
+}
--- src/main/kotlin/su/gachi/listeners/interactions/SlashCommandAutocomplete.kt
+++ src/main/kotlin/su/gachi/listeners/interactions/SlashCommandAutocomplete.kt
@@ -4,11 +4,20 @@ import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInterac
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.interactions.commands.Command
 import su.gachi.items.ArmoryItems
+import su.gachi.items.BackpacksItems
 
 class SlashCommandAutocomplete : ListenerAdapter() {
     override fun onCommandAutoCompleteInteraction(event: CommandAutoCompleteInteractionEvent) {
         if (event.name == "armors" && event.focusedOption.name == "id") {
             val choices = ArmoryItems.getAllIDs()
+                .filter { item -> item.startsWith(event.focusedOption.value) }
+                .map { item -> Command.Choice(item, item) }
+                .toMutableList()
+            choices.add(Command.Choice("list", "list"))
+
+            event.replyChoices(choices).queue()
+        } else if (event.name == "backpacks" && event.focusedOption.name == "id") {
+            val choices = BackpacksItems.getAllIDs()
                 .filter { item -> item.startsWith(event.focusedOption.value) }
                 .map { item -> Command.Choice(item, item) }
                 .toMutableList()
